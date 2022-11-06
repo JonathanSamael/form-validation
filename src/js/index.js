d6171a9ae0e470acb5d430adfb9381887b175e4d
Jonathan Amaral <jonathansamael@live.com>
2022-11-06 17:57:39 -0300

    "first commit"

---
+++ src/js/index.js
@@ -0,0 +1,74 @@
+/* 
+
+A validação do formulário deve ser feita com
+javascript puro.
+
+Ao clicar para enviar o formulário, se caso
+algum campo não estiver preenchido, a borda
+do input deve ficar vermelha e uma mensagem
+de "campo obrigatório" deve aparecer embaixo
+do campo que não foi preenchido, conforme o
+figma.
+
+
+*/
+const form = document.getElementById("form");
+const inputName = document.getElementById("name");
+const inputEmail = document.getElementById("email");
+const inputPhone = document.getElementById("phone");
+const messageText = document.getElementById("message");
+const textError = document.querySelectorAll("small");
+
+form.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    checkInputs();
+});
+
+function checkInputs() {
+    const inputNameValue = inputName.value
+    const inputEmailValue = inputEmail.value
+    const inputPhoneValue = inputPhone.value
+    const messageTextValue = messageText.value
+
+    if (inputNameValue === "") {
+        inputValidationError(inputName)
+        textError[3].classList.add("text-error")
+    } else {
+        inputValidationSuccess(inputName)
+        textError[3].classList.remove("text-error")
+    }
+    if (inputEmailValue === "") {
+        inputValidationError(inputEmail)
+        textError[0].classList.add("text-error")
+    } else {
+        inputValidationSuccess(inputEmail)
+        textError[0].classList.remove("text-error")
+    }
+    if (inputPhoneValue === "") {
+        inputValidationError(inputPhone)
+        textError[1].classList.add("text-error")
+    } else {
+        inputValidationSuccess(inputPhone)
+        textError[1].classList.remove("text-error")
+    }
+    if (messageTextValue === "") {
+        inputValidationError(messageText)
+        textError[2].classList.add("text-error")
+    } else {
+        inputValidationSuccess(messageText)
+        textError[2].classList.remove("text-error")
+    }
+
+}
+
+function inputValidationSuccess(div) {
+    const formFilled = div.parentElement;
+    formFilled.classList.add("success")
+}
+function inputValidationError(div) {
+    const formFilled = div.parentElement;
+    formFilled.classList.add("error");
+    formFilled.classList.remove("success")
+}
+
