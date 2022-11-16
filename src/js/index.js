8cac82bdf5a388641fb7be42cc7541708fdb08ba
JonathanSamael <111207396+JonathanSamael@users.noreply.github.com>
2022-11-16 17:11:43 -0300

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,3 @@
-/* 
-
-A validação do formulário deve ser feita com
-javascript puro.
-
-Ao clicar para enviar o formulário, se caso
-algum campo não estiver preenchido, a borda
-do input deve ficar vermelha e uma mensagem
-de "campo obrigatório" deve aparecer embaixo
-do campo que não foi preenchido, conforme o
-figma.
-
-
-*/
 const form = document.getElementById("form");
 const inputName = document.getElementById("name");
 const inputEmail = document.getElementById("email");
